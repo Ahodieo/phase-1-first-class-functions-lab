482e9696619903c0664b14870ed34cc2406efc34
Jahson Diego <jdiego847@gmail.com>
2021-10-14 21:34:45 -0400

    commit tree 22641340b404e4c71fc5d0740bcccbd7ae38d0df

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+const returnFirstTwoDrivers = function() { return drivers.slice(0,2);}
+const returnLastTwoDrivers = function() {return drivers.slice(2,4);}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = function(fareMultiplier) {
+    return function(fare){ return fareMultiplier * fare;}
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+function selectDifferentDrivers(drivers, fun){
+    return fun(drivers);
+}
